996ed18be78465a5f40c59beff8d5d2523cdd7d9
Rustanov Ilnaz <ilnazrustanov1@gmail.com>
2024-10-10 11:16:15 +0500

    add overrided equals and hashcode

--- src/main/java/hiber/model/Car.java
+++ src/main/java/hiber/model/Car.java
@@ -1,6 +1,7 @@
 package hiber.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "cars")
@@ -51,5 +52,16 @@ public class Car {
         this.series = series;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return series == car.series && Objects.equals(carId, car.carId) && Objects.equals(model, car.model) && Objects.equals(user, car.user);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(carId, model, series, user);
+    }
 }
